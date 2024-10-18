c0d1ae79089f142eb61615960910dcff9118b4c1
zjx <zkertone@gmail.com>
2024-10-18 13:36:51 +0800

    0.4 1.建立统一响应码

--- src/main/java/zjx/maven/project/wind_player_serve/service/ResultCode.java
+++ src/main/java/zjx/maven/project/wind_player_serve/service/ResultCode.java
@@ -7,7 +7,8 @@ public enum ResultCode implements StatusCode {
     SUCCESS(200,"请求成功"),
     FAILED(400,"请求失败"),
     USEREERROR(401,"用户已存在"),
-    USERTOTHEMEERROR(402,"主题不存在");
+    USERTOTHEMEERROR(402,"主题不存在"),
+    TEST(10000,"test");
 
     private int code;
     private String msg;
