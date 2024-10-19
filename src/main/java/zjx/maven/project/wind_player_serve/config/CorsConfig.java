76d07adc5c9b242e6191104ba75707447c9fd6ee
zjx <zkertone@gmail.com>
2024-10-19 20:27:43 +0800

    跨域访问

--- src/main/java/zjx/maven/project/wind_player_serve/config/CorsConfig.java
+++ src/main/java/zjx/maven/project/wind_player_serve/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")//项目中的所有接口都支持跨域
                 .allowedOriginPatterns("*")//所有地址都可以访问，也可以配置具体地址
-                .allowCredentials(true)
+                .allowCredentials(true)//允许浏览器携带凭证（如 cookies 或 HTTP 认证信息）进行跨域请求
                 .allowedMethods("*")//"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"
                 .maxAge(3600);// 跨域允许时间
     }
